527a346573a67f05d3ccdb05c820f9866bb7e1f7
tncrazvan <tangent.jotey@gmail.com>
2023-05-19 19:54:54 +0200

    renaming main.ts to main.js (ts is still supported) and adding some alias names

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
 import { defineConfig } from 'vite';
 import { VitePluginNode } from 'vite-plugin-node';
+import path from 'path'
+import { fileURLToPath } from 'url'
+const file = fileURLToPath(import.meta.url)
+const dir = path.dirname(file).replace(/\\+/, '/')
 
 export default defineConfig({
   root: '.',
@@ -7,14 +11,22 @@ export default defineConfig({
     outDir: 'dist',
     minify: true,
   },
+  resolve: {
+    alias: {
+      '@t': `${path.resolve(dir, 'src/lib/@types')}`,
+      '@scripts': `${path.resolve(dir, 'src/lib/@scripts')}`,
+      '@constants': `${path.resolve(dir, 'src/lib/@constants.js')}`,
+      '@assets': `${path.resolve(dir, 'src/lib/@assets.tjss')}`,
+    },
+  },
   plugins: [
     ...VitePluginNode({
       // tell the plugin where is your project entry
-      appPath: './src/main.ts',
+      appPath: './src/main.js',
 
       // Optional, default: 'viteNodeApp'
       // the name of named export of you app from the appPath file
-      exportName: 'svelteContract',
+      exportName: 'app',
 
       // Optional, default: 'esbuild'
       // The TypeScript compiler you want to use
